df0a87093b62560d6b4b43c5d552f5674f135f8a
Victoria Martinez <vmartinez72@live.com>
2018-11-09 09:24:23 -0800

    correct syntax and value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //business logic for contact/personal information
 function PizzaOrder() {
-  this.contacts = []
+  this.contacts = [];
 }
 
 PizzaOrder.prototype.addContact = function(contact) {
@@ -10,87 +10,86 @@ PizzaOrder.prototype.addContact = function(contact) {
 function Contact(firstName, lastName, phoneNumber, address) {
   this.firstName = firstName,
   this.lastName = lastName,
-  this.phoneNumber = phoneNumber,
-  this.address = address
-
+  this.address = address,
+  this.phoneNumber = phoneNumber
 }
 
 Contact.prototype.personalInfo = function() {
   return this.firstName + " " + this.lastName + "." + " You're address is " + this.address + " and your contact number is " + this.phoneNumber;
-
 }
- var pizzatoppings = ["Pepperoni", "Sausage", "Bacon", "Onion", "Mushroom", "Tomato"];
 
 //business logic for BYO Pizza
-function Pizza(pizzaSize, pizzaSauce, pizzatopping) {
+// var pizzaSize = { size: "Small", "Medium", "Large"} ;
+// var pizzaSauce = { sauce: "Marina", "Alfredo", "Pesto"};
+// var pepperoni = { topping: "pepperoni"};
+// var sausage = { topping: "sausage"};
+// var bacon = { topping: "bacon"};
+// var onion = { topping: "onion"};
+// var mushroom = { topping: "mushroom"};
+// var tomato = { topping: "tomato"};
+var pizzatoppings = ["Pepperoni", "Sausage", "Bacon", "Onion", "Mushroom", "Tomato"];
+
+
+
+
+function Pizza(pizzaSize, pizzaSauce, pizzaTopping) {
   this.pizzaSize = pizzaSize,
   this.pizzaSauce = pizzaSauce,
-  this.pizzaTopping = pizzatopping
+  this.pizzaTopping = pizzaTopping
+
 }
 
 Pizza.prototype.size = function() {
   for (var i=0; i< this.pizzaSize.length; i++) {
-    if (this.pizzaSize[i] === this.pizzaSize)
-    return;
+    if (pizzasize[i] === this.pizzaSize) {
+    }
   }
- return;
+    return this.pizzaSize;
 }
 
-Pizza.prototype.sauce = function() {
+Pizza.prototype.sauce = function(){
   for (var i=0; i< this.pizzaSauce.length; i++) {
-    if (this.pizzaSauce[i] === this.pizzaSauce)
-    return;
+    if (pizzasauce[i] === this.pizzaSauce) {
+      return this.pizzaSauce + 1 ;
+    }
   }
- return;
 }
 
 Pizza.prototype.topping = function() {
-  for (var i=0; i< this.pizzatoppings.length; i++) {
-    if (this.pizzatoppings[i] === this.pizzatopping)
-    return 1;
+  for (var i=0; i< this.pizzaToppings.length; i++) {
+    if (pizzaToppings[i] === this.pizzaTopping) {
+    }
   }
- return 0;
+    return this.pizzaTopping;
 }
 
 Pizza.prototype.getPizzaOrderTotal = function() {
-  return this.pizzaSize + this.pizzaSauce + this.pizzaTopping;
+    return this.pizzaSize + this.pizzaSauce + this.pizzaTopping;
 }
-
-
-
-
 //User Interface
-
-var pizzaOrder = new PizzaOrder();
-
 $(document).ready(function(){
   $("#pizza-order").submit(function(event) {
+    debugger;
     event.preventDefault();
-
-
     var firstName = $("#first-name").val();
     var lastName = $("#last-name").val();
     var phoneNumber = $("#phone-number").val();
     var physicalAddress = $("#physical-address").val();
-    var newContact = new Contact(firstName, lastName, phoneNumber, physicalAddress);
-    var pizzaOrder = newContact.personalInfo();
-    debugger;
 
-    $("#first-name").text(firstName);
-    $("#last-name").text(lastName);
-    $("#physical-address").text(physicalAddress);
-    $("#phone-number").text(phoneNumber);
+    $(".first-name").text(firstName);
+    $(".last-name").text(lastName);
+    $(".physical-address").text(physicalAddress);
+    $(".phone-number").text(phoneNumber);
 
 
     var pizzaSize = parseInt($("input:radio[name=size]:checked").val());
     var pizzaSauce = parseInt($("input:radio[name=sauce]:checked").val());
     var pizzaTopping  = parseInt($("input:radio[name=topping]:checked").val());
-    var pizza = new Pizza(pizzaSize, pizzaSauce, pizzaTopping)
-    var orderTotal = pizza.getPizzaOrderTotal()
+    var pizza = new Pizza(pizzaSize, pizzaSauce, pizzaTopping);
+    var orderTotal = pizza.getPizzaOrderTotal();
+console.log(pizza);
 
-
-      $("#show-result").text(pizzaOrder);
-      $("#pizzaOrder").text(orderTotal)
-
-  });
+    $("#show-result").show();
+    $("#pizzaOrder").text(orderTotal);
+  })
 });
